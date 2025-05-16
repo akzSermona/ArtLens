05245f1e6d0469a8f9c1b4a5dd97f8769232186d
m.sulmona <m.sulmona1@studenti.unimol.it>
2025-05-16 09:50:28 +0200

    add the base layout of HistoryScreen

---
+++ lib/screens/history_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HistoryScreen extends StatefulWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Storia delle tue scansioni',
+          style: TextStyle(fontFamily: 'Limelight', fontSize: 18),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [IconButton(icon: const Icon(Icons.search), onPressed: () {})],
+      ),
+    );
+  }
+}
