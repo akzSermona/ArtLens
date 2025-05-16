fe342886ca1c98b059c147311131ed7ed2c9cef1
m.sulmona <m.sulmona1@studenti.unimol.it>
2025-05-16 09:51:12 +0200

    add the base layout of SavedScreen

---
+++ lib/screens/saved_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SavedScreen extends StatefulWidget {
+  const SavedScreen({super.key});
+
+  @override
+  State<SavedScreen> createState() => _SavedScreenState();
+}
+
+class _SavedScreenState extends State<SavedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Le tue sansioni salvate',
+          style: TextStyle(fontFamily: 'Limelight', fontSize: 18),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [IconButton(icon: const Icon(Icons.search), onPressed: () {})],
+      ),
+    );
+  }
+}
